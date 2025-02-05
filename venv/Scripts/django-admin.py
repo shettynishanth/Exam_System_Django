c01fef2306613923ee8571329fc0ecba72cf4107
shettynishanth <shettynishanth100@gmail.com>
2025-02-05 14:41:26 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\INTERNSHIP1\Full-Fledged-Online-Examination-System\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
